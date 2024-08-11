38714441011801138385b6a560a201817cbe4114
Pew <hi@pew.dev>
2024-08-11 23:07:00 +0800

    Add default items to ItemList.jsx

---
+++ src/lib/constants.js
@@ -0,0 +1,24 @@
+export const initialItems = [
+  {
+    id: 1,
+    name: "Good mood",
+    packed: true,
+  },
+  {
+    id: 2,
+    name: "Passport",
+    packed: false,
+  },
+  {
+    id: 3,
+    name: "Phone charger",
+    packed: false,
+  },
+];
+
+export const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
